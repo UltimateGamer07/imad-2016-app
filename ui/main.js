445bbed88263b6b171ca23854347dc7c69fb6cff
UltimateGamer07 <harinandan.alukkal@gmail.com>
2016-09-25 05:47:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,1 @@
 console.log('Loaded!');
-var red = [0, 100, 63];
-var orange = [40, 100, 60];
-var green = [75, 100, 40];
-var blue = [196, 77, 55];
-var purple = [280, 50, 60];
-
-var myName = "Hari";
-var letterColors = [red,orange,green];
-if (10<9) {bubbleShape = "square"} 
-else {bubbleShape = "circle"}
-
-drawName(myName, letterColors);
-bounceBubbles();
