5dbc404b0ce54e8397b813ca06994966caa3449e
UltimateGamer07 <harinandan.alukkal@gmail.com>
2016-09-25 06:02:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+//Experiments
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];                                     
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "UltimateGamer";
+ 
+letterColors = [red, orange, green, blue, purple];
+
+bubbleShape = "circle";
+drawName(myName, letterColors);
+bounceBubbles();
