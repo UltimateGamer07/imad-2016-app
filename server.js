cd410c156baf1e8fcc40639474e5263039f5007e
UltimateGamer07 <harinandan.alukkal@gmail.com>
2016-09-28 14:38:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,21 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-//Experiments
 
-var red = [0, 100, 63];
-var orange = [40, 100, 60];                                     
-var green = [75, 100, 40];
-var blue = [196, 77, 55];
-var purple = [280, 50, 60];
-
-var myName = "UltimateGamer";
- 
-letterColors = [red, orange, green, blue, purple];
-
-bubbleShape = "circle";
-drawName(myName, letterColors);
-bounceBubbles();
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
