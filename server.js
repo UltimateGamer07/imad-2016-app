8c8776bebbbdd0f3e5c936eef0a8507a2dc683af
UltimateGamer07 <harinandan.alukkal@gmail.com>
2016-09-28 15:08:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
+app.get('/ui/game-1.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'game-1.html'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
